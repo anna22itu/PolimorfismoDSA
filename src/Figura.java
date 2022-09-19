d2c62f132cf84a9f67cd7fc061620b85b1f7bd2b
GPurti <guillem.purti@estudiantat.upc.edu>
2022-09-19 13:39:34 +0200

    Programa definitiu

--- src/Figura.java
+++ src/Figura.java
@@ -1,9 +1,9 @@
 
 // Se nos define la clase Figura como abstracta y que implementa a la interficie Comparable
-public abstract class Figura implements Comparable{ // La clase es abstracta xq el método es abstracto
+public abstract class Figura implements Comparable<Figura>{ // La clase es abstracta xq el método es abstracto
     public abstract double area(); // el método es abstract: ahora no lo implementamos solo lo definimos: devuelve un double
-
+    @Override
     public int compareTo (Figura f){
-        return Double.compareTo(this.area(),f.area());
+        return Double.compare(f.area(),this.area());
     }
 }
--- src/GestorFiguras.java
+++ src/GestorFiguras.java
@@ -19,9 +19,9 @@ public class GestorFiguras{ // Definimos una clase 'GestorFiguras'
 
 
     // Creamos un método para ordenar las áreas de nuestro vector (No devuelve nada)
-    public static void sort(double[] vectorArea)
+    public static void sort(Figura[] vectorFigures)
     {
-        Arrays.sort(vectorArea);
+        Arrays.sort(vectorFigures);
     }
 
 
@@ -31,35 +31,22 @@ public class GestorFiguras{ // Definimos una clase 'GestorFiguras'
         Figura[] vectorFiguras = new Figura[6];
 
         vectorFiguras[0] = new Rectangle(5,10); // le entramos los dos lados
-        System.out.println(vectorFiguras[0].area());
         vectorFiguras[1] = new Cercle(3); // le entramos el radio
-        System.out.println(vectorFiguras[1].area());
         vectorFiguras[2] = new Quadrat(6); // le entramos los dos lados
-        System.out.println(vectorFiguras[2].area());
         vectorFiguras[3] = new Triangle(5,7); // le entramos la base y altura
-        System.out.println(vectorFiguras[3].area());
         vectorFiguras[4] = new Cercle(7); // le entramos el radio
-        System.out.println(vectorFiguras[4].area());
         vectorFiguras[5] = new Quadrat(8); // le entramos los dos lados
-        System.out.println(vectorFiguras[5].area());
-
-
-        //Double[] vectorArea= new Double[6];
-        double[] vectorArea;
-        vectorArea = new double[6];
-        //vectorArea = {vectorFiguras[0].area(),vectorFiguras[1].area(),vectorFiguras[2].area(),vectorFiguras[3].area(),vectorFiguras[4].area(),vectorFiguras[5].area()};
-        vectorArea[0] = vectorFiguras[0].area();
-        vectorArea[1] = vectorFiguras[1].area();
-        vectorArea[2] = vectorFiguras[2].area();
-        vectorArea[3] = vectorFiguras[3].area();
-        vectorArea[4] = vectorFiguras[4].area();
-        vectorArea[5] = vectorFiguras[5].area();
 
         System.out.println(suma(vectorFiguras));
+        for (int i=0; i<6; i++ ){
+            System.out.println(vectorFiguras[i].area());
+        }
+        System.out.print("\n");
+        sort(vectorFiguras);
+        for (int i=0; i<6; i++ ){
+            System.out.println(vectorFiguras[i].area());
+        }
 
-        System.out.println(Arrays.toString(vectorArea));
-        sort(vectorArea);
-        System.out.println(Arrays.toString(vectorArea));
 
     }
 
